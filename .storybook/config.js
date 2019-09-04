c2c09bceb0fc49c2b3f6911f5ac21c594826d88d
HKV <khang.vu@codelink.io>
2019-09-04 16:57:25 +0700

    pls work

--- .storybook/config.js
+++ .storybook/config.js
@@ -13,7 +13,7 @@ function loadStories() {
 addDecorator(
   withOptions({
     name: `react-intl-tel-input v${version}`,
-    url: 'https://github.com/patw0929/react-intl-tel-input',
+    url: 'https://github.com:HKhangVu/react-intl-tel-input.git#7.0.2',
     sidebarAnimations: true,
   })
 );
